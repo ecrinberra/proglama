599ec9af0bc4096365bdd835ca56cea28a184a45
ecrinberra <ecrinberrakagizman@gmail.com>
2024-12-20 12:00:21 +0300

    Add files via upload

---
+++ 241220/ornek1.py
@@ -0,0 +1,5 @@
+not1=int(input("1.sınav notu:"))
+not2=int(input("2.sınav notu:"))
+not3=int(input("3.sınav notu:"))
+ortalama=(not1+not2+not3)/3
+print("notunuzun ortalaması:" ,ortalama)
---
+++ 241220/ornek2.py
@@ -0,0 +1,6 @@
+kisa_kenar=int(input("kisa kenar:"))
+uzun_kenar=int(input("uzun kenar:"))
+cevre=(kisa_kenar+uzun_kenar)*2
+alan=kisa_kenar*uzun_kenar 
+print("Dikdortgenin cevresi:",cevre)
+print("Dikdortgenin cevresi:",alan)
